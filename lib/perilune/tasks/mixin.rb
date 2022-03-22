c2e0ffbb0ce5ba20e3d781aa50c99044ef443d93
Kartavya Pareek <pareekkartavya@gmail.com>
2022-03-22 11:44:56 +0400

    fix: config missing issue for Trifle Stats

--- lib/perilune/tasks/mixin.rb
+++ lib/perilune/tasks/mixin.rb
@@ -62,7 +62,6 @@ module Perilune
         def track_stats(event:, success:)
           Trifle::Stats.track(
             key: "perilune::#{event}", at: Time.zone.now,
-            config: Trifle::Stat.configure,
             values: {
               count: 1,
               success: success ? 1 : 0,
