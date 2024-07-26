8665c2bf12577091b127044ee5855dddf5bf707e
Kostas Kostoglou <kostaskostogloy@outlook.com>
2024-07-26 19:31:40 +0300

    fix: rubocop

--- lib/perilune/engine.rb
+++ lib/perilune/engine.rb
@@ -4,8 +4,8 @@ module Perilune
   class Engine < ::Rails::Engine
     isolate_namespace Perilune
 
-    initializer "perilune.assets.precompile" do |app|
-      app.config.assets.precompile += %w( perilune.js perilune_css.js perilune_css.css )
+    initializer 'perilune.assets.precompile' do |app|
+      app.config.assets.precompile += %w[perilune.js perilune_css.js perilune_css.css]
     end
   end
 end
